91b9b8a46136e6b757f42f36c2ccb5690df16813
Sayuri-Suganuma <sayuri.suganuma@o-g.co.jp>
2024-04-24 12:10:55 +0900

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dev-sugaapp-fe';
+  title = 'Todoリスト';
+  messages: any[] = [];
+  id: number[] = [];
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.LoadMessage();
+  }
+
+  LoadMessage() {
+    const url = 'http://localhost:3000/api/v1/messages';
+    this.httpClient.get(url).subscribe({
+      next: (response: any) => {
+        this.messages= response;
+        console.log(response);
+      }
+  })
+  }
+
+    public postMessageContent(message: string): void {
+    const url = 'http://localhost:3000/api/v1/messages';
+    this.httpClient.post(url, {message}).subscribe({
+      next: (response: any) => {
+        this.LoadMessage();
+        this.clearInput();
+      }
+    })
+  }
+
+  private clearInput(): void {
+    const found =
+  <HTMLInputElement>document.getElementById('message');
+    if(found) {
+      found.value='';
+    }
+  }
+
 }
